83f6428e3966467c215e15f18e7d00fb8dbf9cb6
Анастасия Клюквина <heroes10.n@mail.ru>
2025-01-27 14:33:41 +0300

    немного исправила css

---
+++ script.js
@@ -0,0 +1,22 @@
+//$(document).ready(function(){
+//  $(".hamburger").click(function(){
+//    $(this).toggleClass("hamburger-active");
+//  });
+//});
+
+
+
+
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".nav-links").classList.toggle("show-menu");
+})
+
+
+
+
+let name = "Name";
+
+name.toLowerCase;
+
+name.includes('am');
+
